d1bba0e7bc1f915170c624a92289832147a92013
Oom Lout <aaron@oomlout.com>
2022-10-29 13:28:01 +0100

    Fixed up LED offering.

---
+++ LEDS/0402/X/STAN/W/details.py
@@ -0,0 +1,26 @@
+
+######  Auto translated oomp file
+
+def load(newPart,it):
+    oType = "LEDS"
+    oSize = "0402"
+    oColor = "X"
+    oDesc = "STAN"
+    oIndex = "W"
+    hexID = "L4WW"
+
+    newPart['oompType'].append(oType)
+    newPart['oompSize'].append(oSize)
+    newPart['oompColor'].append(oColor)
+    newPart['oompDesc'].append(oDesc)
+    newPart['oompIndex'].append(oIndex)
+    oompID = oType + "-" + oSize + "-" + oColor + "-" + oDesc + "-" + oIndex 
+    newPart['oompID'].append(oompID)
+
+
+
+    ######  Common
+    newPart['hexID'].append(hexID)
+
+    return newPart
+
